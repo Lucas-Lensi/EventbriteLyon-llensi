75c7b3058e1dd320968c2fac4dc014f964426159
llensi <lucas.lensi@gmail.com>
2019-11-05 15:06:45 +0100

    login finish

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,5 +1,33 @@
 class EventController < ApplicationController
   def index
+    @events = Event.all
+  end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @create_event = Event.new(
+      title: params[:title],
+      description: params[:description],
+      start_date: params[:start_date],
+      duration: params[:duration],
+      price: params[:price],
+      location: params[:location],
+      admin: current_user
+    )
+
+    if @create_event.save
+      flash[:success] = "BRAVO, votre potin a bien été uploadé"
+      redirect_to event_path(@create_event.id)
+    else
+      flash[:error] = "ROLLBACK, hey ! les validations ne sont pas passées, je te renvoie une ou plusieurs erreurs"
+      render :new
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,27 @@ User.destroy_all
 Event.destroy_all
 Attendance.destroy_all
 
-10.times do |index|
+100.times do |index|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   User.create(
-    email: "#{first_name}.#{last_name}@yopmail.com",
+    email: "#{first_name.downcase}.#{last_name.downcase}@yopmail.com",
     encrypted_password: Faker::Internet.password,
     description: Faker::Lorem.paragraph,
     first_name: first_name,
     last_name: last_name
+  )
+  puts "#{index} users created"
+end
+
+30.times do |index|
+  Event.create(
+    start_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+    duration: 120,
+    title: "Event#{index}",
+    description: Faker::Lorem.paragraph,
+    price: Faker::Number.between(from: 1, to: 1000),
+    location: "#{Faker::Address.street_address}, Lyon",
+    admin: User.find(rand(1..100))
   )
 end
